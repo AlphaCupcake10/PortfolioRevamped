c9f191fe5211692ad5d79bc3a015f8be303cd9a6
AlphaCupcake10 <alphacupcake@outlook.com>
2024-03-01 17:35:33 +0530

    Remove unused imports in LeaderboardPage.tsx

--- src/components/GamePage/LeaderboardPage.tsx
+++ src/components/GamePage/LeaderboardPage.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, RefObject, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Navbar from '../Navbar'
 import axios from '../../axios'
 import { gsap } from "gsap";
